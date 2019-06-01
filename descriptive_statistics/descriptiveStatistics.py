ba0a793df08b93506069ab1d63f7cdee360ad170
jcrcs <juancarlosrosito@gmail.com>
2019-06-01 12:20:25 +0200

    add ds to descriptive statisti s

---
+++ descriptive_statistics/descriptiveStatistics.py
@@ -0,0 +1,11 @@
+
+
+class Ds(object):
+    def __init__(self):
+        print("hola")
+
+    def group_byer(self, hola):
+        """this is a groupbier
+        """
+        print("hola")
+        return 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas
+from descriptive_statistics.descriptiveStatistics import *
 
 def main():
+    my_ds = Ds()
+    my_ds.group_byer()
     pass
