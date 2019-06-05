5774a8dc290fa6961f7e5c6fd55816c31dd9ea13
jcrcs <juancarlosrosito@gmail.com>
2019-06-05 04:30:34 +0200

    add RandomForest (not finished) add keras

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import supervised_learning.supervised_learning as sl
+import os
 
 def main():
-    storage = r"./storage/"
-    my_sl = sl.Supervised_learning("storage")
+    #storage = r"./storage/"
+    workspace_dir = get_workspace()
+    my_sl = sl.Supervised_learning(workspace_dir)
     my_sl.run()
 
+def get_workspace():
+    workspace_dir = os.getcwd()
+    return workspace_dir
 main()
--- supervised_learning/supervised_learning.py
+++ supervised_learning/supervised_learning.py
@@ -1,7 +1,18 @@
-# import pandas as pd
+import pandas as pd
+import sklearn
+import sklearn.model_selection
+from sklearn.ensemble import RandomForestClassifier
 
 class Supervised_learning(object):
-    def __init__(self, input_path):
-        print(input_path)
+    def __init__(self, workspace_dir):
+        self.workspace_dir = workspace_dir
+        self.df = pd.read_csv(workspace_dir+r'\\storage\\kag_risk_factors_cervical_cancer.csv')
+        
     def run(self):
         print("run")
+        train, test = sklearn.model_selection.train_test_split(self.df, test_size = 0.3, random_state = 123)
+        train = pd.DataFrame(train)
+        test = pd.DataFrame(test)
+        print(train.describe(include= "all"))
+        mdl = RandomForestClassifier(n_estimators = 100, max_depth = 2, random_state = 123)
+        #mdl.fit()
